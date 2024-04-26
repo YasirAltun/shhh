c771e25083c10dbba1534e732a711ed87d812764
YasirAltun <ayasiraltun@gmail.com>
2024-04-26 10:35:26 +0300

    categorylist.js e kategoriler buton olarak eklendi.

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ListGroup, ListGroupItem } from 'reactstrap'   
+import { ListGroup, ListGroupItem, Button } from 'reactstrap'    
 
 export default class CategoryList extends Component {
   render() {
@@ -8,19 +8,54 @@ export default class CategoryList extends Component {
         <h3>Category List</h3>
         <ListGroup >
   <ListGroupItem  href="/Python/" tag="a">
+  <Button
+    block
+    color="info"
+    outline
+    size="lg"
+  >
     Python
+  </Button>
   </ListGroupItem>
   <ListGroupItem  href="/C++/" tag="a">
+  <Button
+    block
+    color="info"
+    outline
+    size="lg"
+  >
     C++
+  </Button>
   </ListGroupItem>
   <ListGroupItem  href="/Java/" tag="a">
+  <Button
+    block
+    color="info"
+    outline
+    size="lg"
+  >
     Java
+  </Button>
   </ListGroupItem>
   <ListGroupItem  href="/Assembly/" tag="a">
-    Assebmly
+  <Button
+    block
+    color="info"
+    outline
+    size="lg"
+  >
+    Assembly
+  </Button>
   </ListGroupItem>
   <ListGroupItem  href="/C#/" tag="a">
+  <Button
+    block
+    color="info"
+    outline
+    size="lg"
+  >
     C#
+  </Button>
   </ListGroupItem>
 </ListGroup>
       </div>
