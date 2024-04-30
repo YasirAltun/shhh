b881121c415c6f74b3dc85902973e9e2e2aefe00
YasirAltun <ayasiraltun@gmail.com>
2024-04-30 19:59:39 +0300

    butonlar yenilendi video cartları düzenleniyor

--- src/components/CategoriList/CategoryList.js
+++ src/components/CategoriList/CategoryList.js
@@ -3,40 +3,23 @@ import Button from '@mui/material/Button';
 import '@mui/material/colors'
 import './CategoriList.css'
 
+
+
 export default class CategoryList extends Component {
+
+  
   render() {
     return (
       
       <div >
-        <h3 className='.hero'>Category List</h3>
-        
-
-        <div className='button'>
-        <Button  variant="outlined" href='/Python/'>Python</Button> 
-        </div>
-        
-        <div className='button'>
-        <Button variant="outlined" href='/Python/'>Python</Button>
-        </div>
-
-
-        <div className='button'>
-        <Button variant="outlined" href='/Python/'>Python</Button>
-
-        </div>
-        
-        <div className='button'>
-        <Button variant="outlined" href='/Python/'>Python</Button>
-        </div >
-
-        <div className='button'>
-        <Button variant="outlined" href='/Python/'>Python</Button>
-        </div>
-
-
-        <div className='button'>
-        <Button variant="outlined" href='/Python/'>Python</Button>
-        </div>
+        <h3 className='ttl1'>Category List</h3>
+          <button className='btn1'  href='/Python/'>Python</button> 
+          <button className='btn1'  href='/C#/'>C#</button> 
+          <button className='btn1'  href='/C++/'>C++</button> 
+          <button className='btn1'  href='/Assembly/'>Assembly</button> 
+          <button className='btn1'  href='/Java/'>Java</button> 
+          <button className='btn1'  href='/JavaScript/'>JavaScript</button> 
+          <button className='btn1'  href='/HTML/'>HTML</button> 
 
       </div>
     )
--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-
+import './Videos.css'
 export default class Videos extends Component {
   render() {
     return (
       <div>
-        <h3>Videos</h3>
+        <h3 className="ttl1">Videos</h3>
         <div class="container">
-          <div class="row gx-1 justify-content-center">
-            <div class="col-lg-2 col-md-12">12345678912345678</div>
+          
+            <div class="box">
+              <h1>***********</h1>
+              
+            </div>
 
-            <div class="col-lg-2 col-md-9">12345678912345678</div>
+            <div class="box "><h1>***********</h1></div>
 
-            <div class="col-lg-2 col-md-9">12345678912345678</div>
+            <div class="box "><h1>***********</h1></div>
 
-            <div class="col-lg-2 col-md-9">12345678912345678</div>
+            <div class="box "><h1>***********</h1></div>
 
-            <div class="col-lg-2 col-md-9">12345678912345678</div>
-          </div>
+            <div class="box "><h1>***********</h1></div>            
         </div>
       </div>
     );
