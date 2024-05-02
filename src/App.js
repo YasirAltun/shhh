f1a94e4b4fcc77cff895a5bb96a64763bd08372c
YasirAltun <ayasiraltun@gmail.com>
2024-05-03 02:33:20 +0300

    appjs formatlandı sadece, kodlar okunaklı görünsün diye

--- src/App.js
+++ src/App.js
@@ -1,51 +1,40 @@
-import React from 'react'
-import './App.css'
-import Navi from './navbar/Navi'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Python from './Dersler/python'
-import CPP from './Dersler/cpp'
-import Java from './Dersler/java'
-import CSharp from './Dersler/csharp'
+import React from "react";
+import "./App.css";
+import Navi from "./navbar/Navi";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Python from "./Dersler/python";
+import CPP from "./Dersler/cpp";
+import Java from "./Dersler/java";
+import CSharp from "./Dersler/csharp";
 
-
-import Categories2 from './Categories/Categories2'
+import Categories2 from "./Categories/Categories2";
 
 export default function App() {
   return (
-    <section className='hero'> 
-    {/* navbar */}
+    <section className="hero">
+      {/* navbar */}
       <div>
-        <Navi/>
+        <Navi />
       </div>
 
-    {/* kategori listesi */}
-      <Router> 
-        <div >
-            <Categories2/>
-            <Routes>
-                <Route path="./Dersler/python" element={<Python />} />
-                <Route path="./Dersler/csharp" component={CSharp} />
-                <Route path="./Dersler/java" component={Java} />
-                <Route path="./Dersler/cpp" component={CPP} />
-            </Routes>
-          </div> 
-       </Router>
-      
+      {/* kategori listesi */}
+      <Router>
+        <div>
+          <Categories2 />
+          <Routes>
+            <Route path="./Dersler/python" element={<Python />} />
+            <Route path="./Dersler/csharp" component={CSharp} />
+            <Route path="./Dersler/java" component={Java} />
+            <Route path="./Dersler/cpp" component={CPP} />
+          </Routes>
+        </div>
+      </Router>
 
+      {/* Card list */}
+      <div></div>
 
-
-    {/* Card list */}
-      <div>
-
-      </div>
-
-    {/* embed video  */}
-    <div>
-
-    </div>
-
-
+      {/* embed video  */}
+      <div></div>
     </section>
-   
-  )
+  );
 }
